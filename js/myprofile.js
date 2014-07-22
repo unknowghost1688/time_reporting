38da063b4e96abd2ac8955056573681765a48546
unknowghost1688 <unknowghost1688@gmail.com>
2014-07-22 15:30:27 +0800

    b
    b

--- js/myprofile.js
+++ js/myprofile.js
@@ -64,8 +64,8 @@ var myProfile_ShowDetailsFunctions = {
                 NewPassword: newPassword_txt,
                 ConfirmPassword: confirmPassword_txt
             },
-            success: function (result) {
-                $('#reportTo').val(result[0]['LastName'] + " " + result[0]['FirstName']);
+            success: function () {
+                alert('success');
             },
             error: function (jqXHR, status, error) {
                 if (jqXHR.status === 0) {
